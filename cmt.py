48542f1a98638b9cdaaba4ccf84164ecc8d905c7
Yashwanth R <89150253+yashu1wwww@users.noreply.github.com>
2025-02-27 21:07:09 +0530

    Update cmt.py

--- cmt.py
+++ cmt.py
@@ -2,8 +2,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 import time
-import random
 
 # Twitter accounts (Replace with valid credentials)
 accounts = [
